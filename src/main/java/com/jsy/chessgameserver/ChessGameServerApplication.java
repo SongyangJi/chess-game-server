a1430d6ceead04f586f8d35ea445df57aff07d73
SongyangJi <1539242799@qq.com>
2021-05-14 16:25:01 +0800

    init

---
+++ src/main/java/com/jsy/chessgameserver/ChessGameServerApplication.java
@@ -0,0 +1,13 @@
+package com.jsy.chessgameserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessGameServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChessGameServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jsy/chessgameserver/ChessGameServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsy.chessgameserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessGameServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
