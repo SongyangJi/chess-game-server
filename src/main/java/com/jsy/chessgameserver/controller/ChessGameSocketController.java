dea09e49b188ec113cd6e73df61559c8eb35c533
SongyangJi <1539242799@qq.com>
2021-05-19 23:43:35 +0800

    下棋controller（socket）

--- src/main/java/com/jsy/chessgameserver/controller/ChessGameSocketController.java
+++ src/main/java/com/jsy/chessgameserver/controller/ChessGameSocketController.java
@@ -1,12 +1,20 @@
 package com.jsy.chessgameserver.controller;
 
-import com.jsy.chessgameserver.dto.Message;
-import com.jsy.chessgameserver.dto.endecoder.MessageDecoder;
-import com.jsy.chessgameserver.dto.endecoder.MessageEncoder;
+import com.jsy.chessgameserver.dto.chess.ChessOperation;
+import com.jsy.chessgameserver.dto.chess.GameMessage;
+import com.jsy.chessgameserver.dto.endecoder.GameMessageDecoder;
+import com.jsy.chessgameserver.dto.endecoder.GameMessageEncoder;
+import com.jsy.chessgameserver.service.chess.GameRoom;
+import com.jsy.chessgameserver.service.GameRoomService;
 import com.jsy.chessgameserver.websocket.TopicEndpoint;
-import org.springframework.stereotype.Component;
+import lombok.NonNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
 import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 
 /**
@@ -15,25 +23,50 @@ import javax.websocket.server.ServerEndpoint;
  * @Version 1.0
  * @Description:
  */
-@ServerEndpoint(value = "/topic/{room}",
+@ServerEndpoint(value = "/topic/{roomId}",
         decoders = {
-                MessageDecoder.class},
+                GameMessageDecoder.class},
         encoders = {
-                MessageEncoder.class
+                GameMessageEncoder.class
         })
-@Component
+@Controller
 public class ChessGameSocketController extends TopicEndpoint {
 
+    public static GameRoomService gameRoomService;
+
+    public GameRoom gameRoom;
+
+    @OnOpen
+    @Override
+    public void open(@PathParam("roomId") @NonNull String roomId, Session session) {
+        super.open(roomId, session);
+        gameRoom = gameRoomService.getRoom(roomId);
+    }
 
     @OnMessage
-    public void broker(Message message) {
-        while (true) {
-            broadcastMessage(message);
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+    public void broker(GameMessage message) {
+        switch (message.getOpt()) {
+            //  请求移动
+            case MOVE:
+                if (gameRoom.playChess(message.getDot())) {
+                    GameMessage stepMessage = new GameMessage(message.getDot(), message.getRole(), ChessOperation.STEP);
+                    broker(stepMessage);
+                    if (gameRoom.isGameOver()) {
+                        GameMessage overMessage = new GameMessage(null, message.getRole(), ChessOperation.OVER);
+                        broadcastMessage(overMessage);
+                    }
+                }
+                break;
+            case STEP:
+                break;
+            default:
+                break;
         }
+        broadcastMessage(message);
+    }
+
+    @Autowired
+    public static void setGameRoomService(GameRoomService gameRoomService) {
+        ChessGameSocketController.gameRoomService = gameRoomService;
     }
 }
