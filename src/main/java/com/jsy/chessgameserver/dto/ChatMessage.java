5f91b087b3e91194b10ef8fd399090fc34c401c5
SongyangJi <1539242799@qq.com>
2021-05-19 23:41:17 +0800

    update

--- src/main/java/com/jsy/chessgameserver/dto/ChatMessage.java
+++ src/main/java/com/jsy/chessgameserver/dto/ChatMessage.java
@@ -1,9 +1,9 @@
 package com.jsy.chessgameserver.dto;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+
+import java.util.Date;
 
 /**
  * @Author: Song yang Ji
@@ -17,13 +17,20 @@ import lombok.ToString;
 @Setter
 @NoArgsConstructor
 public class ChatMessage extends Message {
-    String from;
-    String to;
-    String message;
+    private String message;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date date;
+
+    public ChatMessage(String from, String to, String message, Date date) {
+        super(from, to);
+        this.message = message;
+        this.date = date;
+    }
 
     public ChatMessage(String from, String to, String message) {
         super(from, to);
         this.message = message;
+        this.date = new Date();
     }
 }
 
