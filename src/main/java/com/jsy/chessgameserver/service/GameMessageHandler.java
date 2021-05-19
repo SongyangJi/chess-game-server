621f8e249a41e67a84e96c78bc37ee2cd09349da
SongyangJi <1539242799@qq.com>
2021-05-19 23:41:59 +0800

    下棋消息处理器

---
+++ src/main/java/com/jsy/chessgameserver/service/GameMessageHandler.java
@@ -0,0 +1,11 @@
+package com.jsy.chessgameserver.service;
+
+/**
+ * @Author: Song yang Ji
+ * @ProjectName: chess-game-server
+ * @Version 1.0
+ * @Description:
+ */
+
+public interface GameMessageHandler {
+}
---
+++ src/main/java/com/jsy/chessgameserver/service/impl/GameMessageHandlerImpl.java
@@ -0,0 +1,15 @@
+package com.jsy.chessgameserver.service.impl;
+
+import com.jsy.chessgameserver.service.GameMessageHandler;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author: Song yang Ji
+ * @ProjectName: chess-game-server
+ * @Version 1.0
+ * @Description:
+ */
+@Service("GameMessageHandler")
+public class GameMessageHandlerImpl implements GameMessageHandler {
+
+}
