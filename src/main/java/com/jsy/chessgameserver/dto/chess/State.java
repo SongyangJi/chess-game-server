496b791f13cc1ed02fce2b6c75129e993047c625
SongyangJi <1539242799@qq.com>
2021-05-21 18:11:04 +0800

    游戏状态枚举类

---
+++ src/main/java/com/jsy/chessgameserver/dto/chess/State.java
@@ -0,0 +1,21 @@
+package com.jsy.chessgameserver.dto.chess;
+
+/**
+ * @Author: Song yang Ji
+ * @ProjectName: chess-game-server
+ * @Version 1.0
+ * @Description:
+ */
+
+public enum State {
+    START("即将开始"),
+    READY("等待准备"),
+    RUNNING("正在进行"),
+    FINISH("已结束");
+
+    String description;
+
+    State(String description) {
+        this.description = description;
+    }
+}
